48008eca9ef5b6275b47d5e3d2e6291287ed99c7
isaacandsam-coder <isaacmwathi955@gmail.com>
2026-01-07 19:56:40 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Online Gikomba</h1>
+      <p>Affordable clothes and shoes online</p>
+    </div>
+  )
+}
+
+export default App
